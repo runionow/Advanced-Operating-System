ea95f1f6ac6c531f04b28d93dd6bc76daeaffe55
Arun Nekkalapudi <anekkal@hulk.soic.indiana.edu>
2017-08-23 18:41:28 -0400

    Hello World | Assignment 1 | Part 1

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -17,16 +17,17 @@ shellcmd xsh_hello(int nargs, char *args[]) {
 
     /* Print Hello-World*/
     
-	if (strlen(args[1]) > 0) {
-		printf("Hello %s, Welcome to the world of Xinu!!", args[1]);
-		return 0;
+    if ((strlen(args[1]) > 0) && (args[1] != NULL) && (nargs > 1)) {
+	if (nargs > 2){
+        	printf("Unable to get your name too many i/p arguments.\n");
+    	}
+	else {
+		printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
+    	}
     }
-    else if (nargs > 2){
-        printf("Unable to get your name too many i/p arguments");
-        return 0;
-    }
-    else if (nargs == 1){
-        printf("Hello Anonymous, Welcome to the world of Xinu!!");
-        return 1;
-    }
+   else {
+        printf("Missing i/p arguments.\n");
+   }
+
+   return 0;
 }
