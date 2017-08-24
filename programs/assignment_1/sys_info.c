cdff0fae98758ef8795e19f13529e999364afadd
Arun Nekkalapudi <anekkal@hulk.soic.indiana.edu>
2017-08-24 17:46:19 -0400

    Assignment 1 | Part 2 | Code Push from Hulk to Local

--- programs/assignment_1/sys_info.c
+++ programs/assignment_1/sys_info.c
@@ -5,42 +5,53 @@
  #include <stdio.h>
  #include <sys/types.h>
  #include <unistd.h>
- 
- void main (int nargs, char *args[])
- {
-	char *message[2];
-	pid_t  pid1,pid2;
-	  	
-	pid1 = fork();
-	pid2 = fork()
+ #include <string.h>
 
-	flag=pipe(message);
+ int main (int nargs, char *args[])
+ {
+	int message[2],rec_msg;
+	char buffer[80];
+	pid_t  pid1,pid2;
+	char message1[] = "Hello, World \n";  	
+	pid1 = fork();
+	/*pid2 = fork();*/
+        int flag;
+	flag = pipe(message);
 	if(flag == 0){
 		printf("pipe has been successfully created.\n");
 	}
 	else{
-		printf("error in creating a pipe.\n")
+		printf("error in creating a pipe.\n");
 	}
 
 
 	/*for Process ID 1*/
-	if (pid1 == 0){ 
+	if (pid1 == 0){
+		close(message[1]);
 		printf("Child PID 1: %d\n",getpid());
-		if((strlen(args[1])>0) && (args[1] != NULL) && (nargs > 1){
-			execl("/bin/echo","echo","list ","of", "files:\n",NULL);
+		rec_msg = read(message[0],buffer,sizeof(buffer));
+		printf("Recieved Message: %s",rec_msg);
+		printf("argumnents: %s",args);
+		if((strlen(args[1])>0) && (args[1] != NULL) && (nargs > 1)){
+			printf("Rambo");
+			execl("/bin/echo","echo","Hello World");
+			return 0;
 		}
 	}		   
 	else{ 
-		printf("Parent PID 1: %d\n",getpid());	  
+		close(message[0]);
+		printf("Parent PID 1: %d\n",getpid());
+		write(message[1], message1, (strlen(message1)+1));		  
 	}
 
-    /*for Process ID 2*/
-	if (pid2 == 0){ 
-        printf("Child PID 2: %d\n",getpid());
-	}		   
-	else{ 
-		printf("Parent PID 2: %d\n",getpid());	  
-	}
-
+    	/*for Process ID 2*/
 	
+	/*if (pid2 == 0){ 
+        *printf("Child PID 2: %d\n",getpid());
+	*}		   
+	*else{ 
+	*	printf("Parent PID 2: %d\n",getpid());	  
+	}*/
+
+	return 1;	
  }
