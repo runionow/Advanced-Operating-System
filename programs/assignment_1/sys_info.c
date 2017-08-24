04758976efbca23bf3a5293519690f84e00a01e7
Arun Nekkalapudi <anekkal@hulk.soic.indiana.edu>
2017-08-24 14:14:53 -0400

    Assignment 1 | Part 2 | Branch Update

--- programs/assignment_1/sys_info.c
+++ programs/assignment_1/sys_info.c
@@ -1,3 +1,15 @@
 /*
  * @author Arun Nekkalapudi 
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+
+void main(void){
+	pid_t procID;
+	procID = fork();
+	printf("%d\n",&procID);
+    	printf("Hello this is arun\n");
+}
