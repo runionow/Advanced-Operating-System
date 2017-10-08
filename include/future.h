59438963e0ce82ed1ad3514bc8ab354f80a50d4d
Arun Nekkalapudi <anekkal@hulk.soic.indiana.edu>
2017-10-08 02:42:04 -0400

    Assignment 5 | future.h

---
+++ include/future.h
@@ -0,0 +1,31 @@
+#ifndef _FUTURE_H_
+#define _FUTURE_H_  
+
+typedef enum {
+  FUTURE_EMPTY,
+  FUTURE_WAITING,
+  FUTURE_READY
+} future_state_t;
+
+typedef enum {
+  FUTURE_EXCLUSIVE,
+  FUTURE_SHARED,
+  FUTURE_QUEUE
+} future_mode_t;
+
+typedef struct {
+  int value;
+  future_state_t state;
+  future_mode_t mode;
+  pid32 pid;
+  yourtype set_queue;
+  yourtype get_queue;
+} future_t;
+
+/* Interface for the Futures system calls */
+future_t* future_alloc(future_mode_t mode);
+syscall future_free(future_t*);
+syscall future_get(future_t*, int*);
+syscall future_set(future_t*, int);
+ 
+#endif /* _FUTURE_H_ */
